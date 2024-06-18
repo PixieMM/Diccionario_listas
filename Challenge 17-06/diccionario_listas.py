eddafa05f95f33d2d5c5d0c9c783e760b80e0853
PixieMM <majomp7@gmail.com>
2024-06-17 20:35:39 -0400

    Add files via upload

---
+++ Challenge 17-06/diccionario_listas.py
@@ -0,0 +1,9 @@
+# Definir las listas de claves y valores
+claves = ['a', 'b', 'c', 'd']
+valores = [1, 2, 3, 4]
+
+# Crear el diccionario utilizando la función zip y el constructor dict
+diccionario = dict(zip(claves, valores))
+
+# Imprimir el diccionario resultante
+print(diccionario)
